925823fe452ef3f370ed3a6829dd2900a95ea0be
marcvillanueva1 <marcvillanueva2311@gmail.com>
2022-04-15 17:00:02 -0300

    tabla registro

--- app_de_notas_crear_db.sql
+++ app_de_notas_crear_db.sql
@@ -52,53 +52,44 @@ CREATE OR REPLACE TABLE notas(
 	constraint categoria_id_fk foreign key(id_categoria) references categorias(id)
 );
 
-CREATE OR REPLACE TABLE registro(
-id INT NOT NULL AUTO_INCREMENT,
-id_nota INT,
-gestion enum('creacion','modificacion','eliminacion') NOT NULL,
-fecha_de_gestion timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-key registro_id_nota (id_nota),
-constraint registro_id_nota foreign key (id_nota) references notas(id)
-)
-
 INSERT INTO usuarios
-VALUES(12,
+VALUES (1,
   'Marcos',
   'Villanueva',
   'marcvillanueva2311@gmail.com'
-),(23,
+),(2,
 'Agus',
 'Trossero',
 'agustrossero@gmail.com'
-),(34,
+),(3,
 'Juan',
 'Larraz',
 'juanlarraz@gmail.com'
-),(45,
+),(4,
 'Ignacio',
 'Maldonado',
 'ignaciomaldonado@gmail.com'
-),(56,
+),(5,
 'Lucas',
 'Sequeira',
 'lucassequeira@gmail.com'
-),(67,
+),(6,
 'Julian',
 'Hernandez',
 'julihernandez@gmail.com'
-),(78,
+),(7,
 'Mauri',
 'Hunau',
 'maurihunau@gmail.com'
-),(89,
+),(8,
 'Gaston',
 'Elthe',
 'gastonelthe@gmail.com'
-),(910,
+),(9,
 'Luciano',
 'Saeta',
 'luchosaeta@gmail.com'
-),(101,
+),(10,
 'Maria',
 'Gianera',
 'mariagianera@gmail.com'
@@ -201,41 +192,23 @@ VALUES(1,
 0,
 10);
 
+drop table if exists registro;
+create table registro(
+id int primary key NOT NULL AUTO_INCREMENT,
+id_nota int not null,
+gestion enum('creación','modificacion','eliminación') NOT NULL,
+fecha_de_gestion timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+key registro_id_nota_notas_id (id_nota),
+constraint registro_id_nota_notas_id foreign key (id_nota) references notas(id)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO gestiones(id_nota, gestion, fecha_de_gestion )
-VALUES (11,
-'eliminacion',
- '2020-04-20 04:54:40'),    
-(32,
-'creacion',
- '2019-05-13 14:50:11'),
-(43,
-'eliminacion'
-, '2019-12-06 06:52:25'),
-(54,
-'eliminacion',
- '2019-08-11 18:43:59'),
-(65,
-'modificacion',
- '2020-01-25 07:22:05'),
-(76,
-'modificacion',
- '2019-07-06 00:08:08'),
-(87,
-'creacion',
- '2019-09-21 11:44:17'),
-(98,
-'creacion',
- '2019-11-05 10:42:02'),
-(109,
-'modificacion',
- '2019-06-01 22:06:23');
-
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+INSERT INTO registro(id_nota, gestion, fecha_de_gestion )
+VALUES (13,'eliminación','2020-04-20 04:54:40'),
+(31,'creación', '2019-05-13 14:50:11'),
+(42,'eliminación', '2019-12-06 06:52:25'),
+(53,'eliminación', '2019-08-11 18:43:59'),
+(64,'modificacion', '2020-01-25 07:22:05'),
+(72,'modificacion', '2019-07-06 00:08:08'),
+(85,'creación', '2019-09-21 11:44:17'),
+(93,'creación', '2019-11-05 10:42:02'),
+(101,'modificacion', '2019-06-01 22:06:23');
